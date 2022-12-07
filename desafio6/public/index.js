c3ca164adc909508f0776f56f29859493b479a80
lurivero <lucasesteban.r@gmail.com>
2022-12-07 16:51:47 -0300

    cerrando desafio6 -websockets-

---
+++ desafio6/public/index.js
@@ -0,0 +1,57 @@
+const socket = io.connect();
+
+const divProducts = document.getElementById("productsMain");
+const tableProducts = document.getElementById("bodyProducts");
+
+function updateProducts(table, productsArray) {
+  document.getElementById("bodyProducts").innerHTML = "";
+  productsArray.forEach((element) => {
+    let row = table.insertRow();
+    row.insertCell().innerHTML = element.title;
+    row.insertCell().innerHTML = element.price;
+    row.insertCell().innerHTML = `<img src="${element.thumbnail}" alt="${element.title}" width="60px">`;
+  });
+}
+
+function updateMessages(messagesArray) {
+  const html = messagesArray
+    .map((message) => {
+      return `<div>
+      <span style="color: blue">${message.email}</span><span style="color: brown"> [${message.dateAndTime}] </span><span style="color: green">${message.message}</span>
+    </div>`;
+    })
+    .join(" ");
+  document.getElementById("messagesMain").innerHTML = html;
+}
+
+function addProduct() {
+  const product = {
+    title: document.getElementById("title").value,
+    price: document.getElementById("price").value,
+    thumbnail: document.getElementById("thumbnail").value,
+  };
+
+  socket.emit("newProduct", product);
+  return false;
+}
+
+function sendMessage() {
+  const message = {
+    email: document.getElementById("email").value,
+    dateAndTime: new Date(Date.now()).toLocaleString(),
+    message: document.getElementById("message").value,
+  };
+
+  socket.emit("newMessage", message);
+  return false;
+}
+
+socket.on("productos", (data) => {
+  data.length > 0
+    ? updateProducts(tableProducts, data)
+    : (document.getElementById("productsMain").innerHTML = "No products");
+});
+
+socket.on("messages", (data) => {
+  updateMessages(data);
+});
--- desafio6/server.js
+++ desafio6/server.js
@@ -14,3 +14,37 @@ app.set("view engine", "ejs");
 
 const productosContenedor = new Contenedor("productos.txt");
 const mensajesContenedor = new Contenedor("mensajes.txt");
+
+//GET
+app.get("/productos", async (req, res) => {
+  try {
+    const productos = await productosContenedor.getAll();
+    res.render("index", {
+      pageTitle: "Desafio6 - Ejs",
+      productos: productos,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+io.on("connection", async (socket) => {
+  console.log("Nuevo usuario conectado");
+  const productos = await productosContenedor.getAll();
+  const mensajes = await mensajesContenedor.getAll();
+  socket.emit("productos", productos);
+  socket.emit("messages", mensajes);
+
+  socket.on("newProduct", async (data) => {
+    await productosContenedor.save(data);
+    io.sockets.emit("productos", await productosContenedor.getAll());
+  });
+
+  socket.on("newMessage", async (data) => {
+    await mensajesContenedor.save(data);
+    io.sockets.emit("messages", await mensajesContenedor.getAll());
+  });
+});
+
+const PORT = 8080;
+httpServer.listen(PORT, () => console.log("Escuchando en puerto " + PORT));
